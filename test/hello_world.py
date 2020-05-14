80a4546b6fb0f50448be85e7acb98d4f608bcf70
prasant747 <kumar.prasant7@gmail.com>
2020-05-14 15:40:00 +0530

    simplifying test

--- test/hello_world.py
+++ test/hello_world.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!, App banana hua ab aasan"
-
-if __name__ == "__main__":
-    app.run()
+print("ye kya bakchodi hai")
